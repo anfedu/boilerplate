b9a4d94734746662bd012749ea9ef5c19bf874d8
anfedu <firdausnurilahmad@gmail.com>
2021-02-03 15:43:34 +0700

    boiler plate

---
+++ typesGraphqlPostgres/src/index.ts
@@ -0,0 +1,64 @@
+import "reflect-metadata";
+import { GraphQLError, GraphQLFormattedError } from "graphql";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+
+import { redis } from "./redis";
+import { createSchema } from "./utils/createSchema";
+
+const main = async () => {
+  await createConnection();
+
+  const schema = await createSchema();
+
+  const apolloServer = new ApolloServer({
+    schema,
+    formatError: (error: GraphQLError): GraphQLFormattedError => {
+      if (error && error.extensions) {
+        error.extensions.code = "GRAPHQL_VALIDATION_FAILED";
+      }
+      return error;
+    },
+    context: ({ req, res }: any) => ({ req, res }),
+  });
+
+  const app = express();
+
+  const redisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+  );
+
+  app.use(
+    session({
+      store: new redisStore({
+        client: redis as any,
+      }),
+      name: "token",
+      secret: "923898asdfvjoi",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on http://localhost:4000/graphql");
+  });
+};
+
+main();
